e878498039cfe83e5b3e0766cc4e0fddad9e2302
borodache <borodache@gmail.com>
2024-04-26 16:33:10 +0300

    Removing Toxic Comments
    The main feature now works. Now all is left is to delete pictures and audio

--- toxic_moderator_chat.py
+++ toxic_moderator_chat.py
@@ -1,4 +1,4 @@
-from telegram.ext import Updater, CommandHandler, MessageHandler, CallbackContext, Filters
+from telegram.ext import Updater, MessageHandler, CallbackContext, Filters
 from telegram import Update
 from model_wrapper_loader import MultiLabelDetectionModel, load_model
 
@@ -6,48 +6,43 @@ from model_wrapper_loader import MultiLabelDetectionModel, load_model
 model_wrapper = load_model()
 
 
-# def start(update: Update, context: CallbackContext) -> None:
-#     context.bot.send_message(chat_id=update.message.chat_id, text="Hi all, this is a toxic free group chat. Enjoy your time!")
+def get_full_name(message):
+    first_name = message.from_user.first_name
+    first_name = first_name[0].upper() + first_name[1:]
+    last_name = message.from_user.last_name
+    last_name = last_name[0].upper() + last_name[1:]
 
-
-def edit(update: Update, context: CallbackContext, new_text: str) -> None:
-    print("***")
-    print("edit func")
-    print("update:")
-    print(update)
-    print("context:")
-    print(context)
-    print("new_text:")
-    print(new_text)
-    print("***")
-
-    context.bot.editMessageText(chat_id=update.message.chat_id,
-                                message_id=update.message.reply_to_message.message_id,
-                                text=new_text)
+    return first_name + ' ' + last_name
 
 
 # Define a function to handle the messages that the bot receives
 def message_handler(update: Update, context: CallbackContext):
     # Get the message from the update
-    print("---")
-    print("I am in message_handler")
-    print("update: ")
-    print(update)
-    print("context: ")
-    print(context)
-    print("---")
     message = update.message
 
     # Check for image or audio
+    print("message.photo: ")
+    print(message.photo)
+    print("message.audio: ")
+    print(message.audio)
     if message.photo or message.audio:
         message.delete()
-        print(f"Deleted {message.content_type} message")
+        str_not_allowed_data_type = f"Deleted {message.content_type} message"
+        print(str_not_allowed_data_type)
+        context.bot.send_message(chat_id=update.message.chat_id,
+                                 text=str_not_allowed_data_type)
+
         return # Skip further processing for this message
-    
+
     str_prediction = model_wrapper.predict(message.text)
     if str_prediction != "This message was approved":
-        message.text = str_prediction
-        edit(update, context, str_prediction)
+        full_name = get_full_name(message)
+        str_prediction = full_name + ': ' + str_prediction.lower()
+        message.delete()
+        context.bot.send_message(chat_id=update.message.chat_id,
+                                 text=str_prediction)
+        print(str_prediction)
+        return
 
     # Print the message to the console
     print(message.text)
@@ -55,8 +50,6 @@ def message_handler(update: Update, context: CallbackContext):
 
 def main() -> None:
     updater = Updater("TOKEN", use_context=True)
-    # updater.dispatcher.add_handler(CommandHandler('start', start))
-    updater.dispatcher.add_handler(CommandHandler('edit', edit))
     updater.dispatcher.add_handler(MessageHandler(Filters.text, message_handler))
     updater.start_polling()
     updater.idle()
